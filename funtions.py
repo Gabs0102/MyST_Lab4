18025b9647815367cc969f66ba4f2d878c6ac6c3
Gabs0102 <if715478@iteso.mx>
2023-04-25 15:40:50 -0600

    Creación de carpetas

---
+++ funtions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 07:50:23 2023
+
+@author: gabrielaortiz
+
+Hacer dos funciones 
+1. Tiene que remover la media a una columna de pandas.
+
+2.Sumar un numero entero a la columna
+
+"""
+
+def remove_mean(data):
+    return data-data.mean()    
+
+def add_number(data,number):
+    return data+number
+
+
