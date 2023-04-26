0086a4c0a9a83b9715cc85f87c72a07c6977ab72
Gabs0102 <if715478@iteso.mx>
2023-04-25 20:43:56 -0600

    Parte 0, 1 y 2

--- data.py
+++ data.py
@@ -5,3 +5,20 @@ Created on Fri Jan 20 08:03:29 2023
 
 @author: gabrielaortiz
 """
+
+import asyncio
+import ccxt.async_support as ccxta
+import time
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+     exchanges = ["kucoin", "bittrex", "bitfinex"]
+     run_time = 10  # seconds
+     symbol = "ETH/BTC"
+
+     data = asyncio.run(multi_orderbooks(exchanges, run_time=run_time, symbol=symbol))
+     data = [item for sublist in data for item in sublist]
+     data = pd.DataFrame(data)
+
+     print(data.head())
--- funtions.py
+++ funtions.py
@@ -5,12 +5,6 @@ Created on Fri Jan 20 07:50:23 2023
 
 @author: gabrielaortiz
 
-Hacer dos funciones 
-1. Tiene que remover la media a una columna de pandas.
 
-2.Sumar un numero entero a la columna
 
 """
-
-
-
